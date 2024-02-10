9bc9baa0abbb14e4365abafd23e90fc4f92b9a92
Saeed_Mohamed <130864211+SMohamed002@users.noreply.github.com>
2024-02-10 19:04:13 +0200

    app.py

--- app.py
+++ app.py
@@ -1,55 +1,48 @@
 from flask import Flask, request, render_template
 import os
 import numpy as np
-from PIL import Image
 import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+
+# Load the pre-trained neural network model
+model = tf.keras.models.load_model('models/Model100.h5')
 
 app = Flask(__name__)
 app.static_folder = 'static'
 
-# تحميل نموذج الشبكة العصبية المدربة سابقًا
-script_dir = os.path.dirname(os.path.realpath(__file__))
-
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# استقبال الصورة وتصنيفها
-@app.route('/classify', methods=['POST'])
+# Receiving the image and classifying it
+@app.route('/upload', methods=['POST'])
 def classify_image():
     if request.method == 'POST':
-        # استقبال الصورة المُرسلة من التطبيق Flutter
+        # Receiving the image sent from the Flutter application
         img_file = request.files['image']
 
-        # حفظ الصورة مؤقتًا على الخادم
-        img_path = os.path.join(script_dir, 'temp_image.jpg')
+        # Save the image temporarily on the server
+        img_path = 'temp_image.jpg'
         img_file.save(img_path)
 
-        # تحميل النموذج داخل الدالة
-        model_path = os.path.join(script_dir, 'models/Model100.h5')
-        model = tf.keras.models.load_model(model_path)
-
-        # تحميل الصورة وتصنيفها باستخدام النموذج
-        test_image = image.load_img(img_path, target_size=(224, 224))
-        test_image = image.img_to_array(test_image)
+        # Load and classify the image using the model
+        test_image = tf.keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
+        test_image = tf.keras.preprocessing.image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
-        test_image = preprocess_input(test_image)
+        test_image = test_image / 255.0
 
-        # تصنيف الصورة باستخدام النموذج
+        # Classify the image using the model
         predictions = model.predict(test_image)
         predicted_class = np.argmax(predictions)
 
-        # تحويل النتائج إلى الأسماء المعروفة
+        # Convert the results into known class names
         classes = ['ALL', 'AML', 'CLL', 'CML', 'Healthy']
         result = classes[predicted_class]
 
-        # حذف الصورة المؤقتة
+        # Remove the temporary image
         os.remove(img_path)
 
-        # عرض نتيجة التصنيف في الصفحة result.html
-        return render_template('result.html', classification=result)
+        # Return the classification result as HTML
+        return render_template('result.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
